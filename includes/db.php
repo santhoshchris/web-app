b3db6820d009bc9cd858b387f15f27e8b8b1ac8f
chri0163 <chri0163@algonquinlive.com>
2012-08-16 17:41:29 -0400

    DB change for fog

--- includes/db.php
+++ includes/db.php
@@ -5,9 +5,12 @@
 
 // Our username and password are kept in Environment Varialbes, in .htaccess
 // This is for security, so they are never publicly visible on GitHub
-$user = getenv('DB_USER'); // The MySQL username
-$pass = getenv('DB_PASS'); // The MySQL password
-$data_source = getenv('DATA_SOURCE');
+$user = getenv('MYSQL_USERNAME'); // The MySQL username
+$pass = getenv('MYSQL_PASSWORD'); // The MySQL password
+$host = getenv('MYSQL_DB_HOST');
+$name = getenv('MYSQL_DB_NAME');
+
+$data_source = sprintf('mysql:host=%s;dbname=%s', $host, $name);
 
 // PDO: PHP Data Objects
 // Allows us to connect to many different kinds of databases
@@ -16,3 +19,4 @@ $db = new PDO($data_source, $user, $pass);
 // Force the connection to communicate in UTF-8
 // and support many human languages
 $db->exec('SET NAMES utf8');
+
